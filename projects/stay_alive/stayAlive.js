aa5d1f10924c5f479dcb8076fd5723fd2afa17d0
Andrei Cozma <andreig992@gmail.com>
2016-09-06 16:41:18 -0500

    Fixes

--- projects/stay_alive/stayAlive.js
+++ projects/stay_alive/stayAlive.js
@@ -1,135 +1,256 @@
 var canvas = document.getElementById("myCanvas");
-// ^ SETS UP THE CANVAS. THE CANVAS IS THE WINDOW WHERE ALL OBJECTS OF THE GAME ARE PLACED
 
-resizeCanvas();
-// ^ CALLS THE resizeCanvas() FUNCTION BELOW, WHICH EXECUTES ALL THE CODE INSIDE OF THE FUNCTION
 
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-// ^ THIS FUNCTION SETS THE CANVAS WIDTH AND HEIGHT TO MATCH THE INNER WIDTH AND HEIGHT OF YOUR BROWSER
 
-window.addEventListener("resize", function() {
-    resizeCanvas();
-});
-// ^ THIS GETS TRIGGERED EVERY TIME THE USER RESIZES THE BROWSER WINDOW
-// ^ IF YOU RESIZE THE BROWSER WINDOW, THE resizeCanvas() FUNCTION GETS CALLED AGAIN, ALLOWING IT TO MATCH THE CHNAGED WIDTH AND HEIGHT OF THE BROWSER
+canvas.width = window.innerWidth;
+
+canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext("2d");
-// ^ SETS THE CONTEXT OF THE CANVAS, TELLING IT THAT WE'LL BE DRAWING 2D OBJECT ON IT.
 
 var cursorX;
+
 var cursorY;
 
 var squareSizeX = 10;
+
 var squareSizeY = 10;
 
 var circles = [];
-var circleSize = 10;
-var numberofCircles = 2;
-//^ INITIALIZES VARIABLE THAT WILL BE USED TO DETERMINE CERTAIN THINGS WE'LL BE USING TO MAKE THE GAME WORK
 
-var isGameOver = false;
-//^ THIS VARIABLE WILL BE USED TO DETERMINE WHEN THE GAME SHOULD STOP. THE GAME WILL CONTINUE DRAWING AS LONG AS THIS IS FALSE.
+var circleSize = 10;
+
+var numberofCircles = 2;
+
+
+
+
 
 function createSquare(x, y) {
-    // ^ THE x AND y VALUES GIVEN ABOVE ARE USED BELOW TO TELL WHERE THE SQUARE SHOULD BE DRAWN
-    ctx.beginPath();
-    ctx.rect(x, y, squareSizeX, squareSizeY);
-    //^ THE x AND y VALUES ARE USED THERE TO DRAW THE OBJECT IN THAT SPOT
-    ctx.fillStyle = "#FF0000";
-    //^ THIS SETS THE COLOR OF THE OBJECT
-    ctx.fill();
-    //^ TELLS THAT THE OBJECT SHOULD BE FILLED INSIDE, INSTEAD OF ONLY HAVING AN OUTLINE
-    ctx.closePath();
-    //^ FINISHES THE DRAWING OF THE OBJECT
+
+  ctx.beginPath();
+
+  ctx.rect(x, y, squareSizeX, squareSizeY);
+
+  ctx.fillStyle = "#FF0000";
+
+  ctx.fill();
+
+  ctx.closePath();
+
 }
-// ^ THIS IS THE BASIC FUNCTION THAT OS CALLED EACH TIME YOU WANT TO CREATE A NEW SQUARE
+
 
 function createCircle(x, y) {
-    // ^ THE x AND y VALUES GIVEN ABOVE ARE USED BELOW TO TELL WHERE THE CIRCLE SHOULD BE DRAWN
-    ctx.beginPath();
-    ctx.arc(x, y, circleSize, 0, Math.PI * 2);
-    //^ THE x AND y VALUES ARE USED THERE TO DRAW THE OBJECT IN THAT SPOT
-    ctx.fillStyle = "#0095DD";
-    //^ THIS SETS THE COLOR OF THE OBJECT
-    ctx.fill();
-    //^ TELLS THAT THE OBJECT SHOULD BE FILLED INSIDE, INSTEAD OF ONLY HAVING AN OUTLINE
-    ctx.closePath();
-    //^ FINISHES THE DRAWING OF THE OBJECT
+
+  ctx.beginPath();
+
+  ctx.arc(x, y, circleSize, 0, Math.PI * 2);
+
+  ctx.fillStyle = "#0095DD";
+
+  ctx.fill();
+
+  ctx.closePath();
+
 }
-// ^ THIS IS THE BASIC FUNCTION THAT OS CALLED EACH TIME YOU WANT TO CREATE A NEW CIRCLE
+
+
+function getMousePos(canvas, evt) {
+
+  var rect = canvas.getBoundingClientRect();
+
+  return {
+
+    x: evt.clientX - rect.left,
+
+    y: evt.clientY - rect.top
+
+  };
+
+}
 
 for (var i = 0; i < numberofCircles; i++) {
-    addCircles();
+  addCircles();
 }
+
+
+
+var isGameOver = false;
+var shouldDisplayMouseMove = true;
+
+var noMoveTimer = 4;
+
 
 function drawingLoop() {
-    if (isGameOver == false) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        for (var i = 0; i < circles.length; i++) {
-            createCircle(circles[i][0], circles[i][1]);
-        }
-        createSquare(cursorX - squareSizeX / 2, cursorY - squareSizeY / 2);
-        ctx.font = "25px Arial";
-        ctx.textAlign = "center";
-        ctx.fillText("HOW LONG CAN YOU STAY ALIVE?", canvas.width / 2, 30);
-        ctx.fillText(circles.length, canvas.width / 2, 60);
+  ctx.font = "25px Arial";
+    ctx.textAlign = "center";
+  if (isGameOver === false) {
+
+    clearCanvas();
+       for (var i = 0; i < circles.length; i++) {
+
+      createCircle(circles[i][0], circles[i][1]);
+
     }
+
+    createSquare(cursorX, cursorY);
+
+
+
+    ctx.fillText("HOW LONG CAN YOU STAY ALIVE?", canvas.width / 2, 30);
+
+    ctx.fillText(circles.length, canvas.width / 2, 60);
+
+    if (shouldDisplayMouseMove === true) {
+      ctx.fillText("MOVE YOUR MOUSE! YOU'LL DIE IN " + noMoveTimer, canvas.width / 2, canvas.height / 2);
+    }
+    }
+  
 }
 
+
+
+function clearCanvas() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+function displayMoveText() {
+  shouldDisplayMouseMove = true;
+  if(shouldDisplayMouseMove){
+    noMoveTimer = noMoveTimer - 1;
+      
+  
+
+  if (noMoveTimer === 0) {
+    clearCanvas();
+    gameOver("YOU WERE IDLE FOR TOO LONG! KEEP MOVING YOUR MOUSE");
+  }
+  }
+ 
+ 
+}
+setInterval(displayMoveText, 2000);
+
+
+
+function gameOver(text) {
+ 
+  isGameOver = true;
+  ctx.fillText(text, canvas.width / 2, canvas.height / 2);
+}
 
 
 canvas.addEventListener('mousemove', function(evt) {
 
-    var mousePos = getMousePos(canvas, evt);
-    cursorX = mousePos.x;
-    cursorY = mousePos.y;
-    // ^ EVERY TIME YOU MOVE YOUR MOUSE, THE cursorX and cursorY VARIABLES GET UPDATED WITH THE NEW MOUSE COORDINATES
+ 
 
-    for (var i = 0; i < circles.length; i++) {
-        var closenessX = Math.abs(circles[i][0] - mousePos.x);
-        var closenessY = Math.abs(circles[i][1] - mousePos.y);
-        if (closenessX < circleSize && closenessY < circleSize) {
-            console.clear();
-            console.log("touched " + closenessX);
-            ctx.font = "30px Arial";
-            isGameOver = true;
-            ctx.fillText("YOU DIED! CLICK TO TRY AGAIN!", canvas.width / 2, canvas.height / 2);
-        }
+  shouldDisplayMouseMove = false;
+  noMoveTimer = 4;
+
+
+  var mousePos = getMousePos(canvas, evt);
+
+
+
+  cursorX = mousePos.x - squareSizeX / 2;
+
+  cursorY = mousePos.y - squareSizeY / 2;
+
+
+
+  for (var i = 0; i < circles.length; i++) {
+
+    var closenessX = Math.abs(circles[i][0] - mousePos.x);
+
+    var closenessY = Math.abs(circles[i][1] - mousePos.y);
+
+
+
+    if (closenessX < circleSize && closenessY < circleSize) {
+
+      console.clear();
+
+      console.log("touched " + closenessX);
+
+
+      gameOver("YOU DIED! CLICK TO TRY AGAIN");
+
     }
-});
-// ^ EVERY TIME YOU MOVE YOUR MOUSE INSIDE THE CANVAS, THIS GETS CALLED
 
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
-    return {
-        x: evt.clientX - rect.left,
-        y: evt.clientY - rect.top
-    };
-}
-//^ CALLING THIS FUNCTION RETURS BACK X AND Y VALUES FOR THE MOUSE POINTER COORDINATES
+  }
+
+
+});
+
+
 
 canvas.addEventListener("click", function() {
-    if (isGameOver) {
-        circles = [];
-        isGameOver = false;
-    }
+
+  if (isGameOver) {
+
+    circles = [];
+
+    isGameOver = false;
+
+  }
+
 });
 
+
+
+
+
+
+
 setInterval(drawingLoop, 1);
+
 var intervalAddCircles = 500;
 
+
+
 function addCircles() {
-    var circlesX;
-    var circlesY;
-    var circleCoords = [0, 0];
-    circlesX = Math.random() * (canvas.width - 10) + 10;
-    circlesY = Math.random() * (canvas.height - 10) + 10;
+
+
+
+  var circlesX;
+
+  var circlesY;
+
+  var circleCoords = [0, 0];
+
+
+
+  circlesX = Math.random() * (canvas.width - 10) + 10;
+
+  circlesY = Math.random() * (canvas.height - 10) + 10;
+
+  if(Math.abs(circlesX - cursorX) < 50 && Math.abs(circlesY - cursorY) < 50 ){
+   
+  } else{
     circleCoords[0] = circlesX;
-    circleCoords[1] = circlesY;
-    circles.push(circleCoords);
+
+  circleCoords[1] = circlesY;
+
+  circles.push(circleCoords);
+  }
+ 
+
 }
 
+
+
 var intervalID = setInterval(addCircles, 100);
+
+
+
+
+
+window.addEventListener("resize", function() {
+
+  canvas.width = window.innerWidth;
+
+  canvas.height = window.innerHeight;
+
+});
